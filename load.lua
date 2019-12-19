8fc7c24437c3234ec0a8646ccaddd5503c05d4f9
HaNaK0 <saturn-v@hotmail.com>
2019-12-19 20:32:40 +0100

    First sprite drawn!

--- load.lua
+++ load.lua
@@ -1,15 +1,42 @@
-Sp1 = {}
+local Sp1 = {}
 
 function Sp1.LoadSprites()
-    Sp1.sprites = {}
+	local function LoadSpriteList(tableOfPaths)
+		local sprites = {}
+		local spriteNames = {}
 
-    Sp1.sprites.Square = love.graphics.newImage("Sprites/Square.png")
+		sprites.names = spriteNames
+
+		for i,v in ipairs(tableOfPaths)
+		do
+			sprites[i] = love.graphics.newImage(v)
+			spriteNames[i] = v:match("^.+/(.+)$")
+		end
+
+		return sprites
+	end
+
+	local sprites = {}
+
+	sprites.gems = LoadSpriteList(
+		{"Sprites/png/element_blue_diamond_glossy.png", 
+		"Sprites/png/element_green_polygon_glossy.png", 
+		"Sprites/png/element_grey_square_glossy.png",
+		"Sprites/png/element_purple_diamond_glossy.png",
+		"Sprites/png/element_red_polygon_glossy.png",
+		"Sprites/png/element_yellow_square_glossy.png"
+	})
+
+	sprites.slector = LoadSpriteList({"Sprites/png/selectorA.png", "Sprites/png/selectorB.png"})
+
+	return sprites
 end
 
 function Sp1.GameSetup(sizeX, sizeY)
-    Sp1.game = {}
-    local game = Sp1.game
+	Sp1.game = {}
+	local game = Sp1.game
 
-    game.size = {x = sizeX, y = sizeY}
-    
+	game.size = {x = sizeX, y = sizeY}
 end
+
+return Sp1
--- main.lua
+++ main.lua
@@ -1,12 +1,19 @@
+Game = require("load")
 
-function love.load(arg)
-
+function love.load(args)
+	Game.sprites = Game.LoadSprites()
+	number = 0;
+	timer = 0;
 end
 
 function love.update(deltaTime)
-
+	timer = timer + deltaTime;
+	if timer > 1 then
+		number = (number + 1) % table.getn(Game.sprites.gems)
+		timer = 0
+	end
 end
 
 function love.draw()
-
+	love.graphics.draw(Game.sprites.gems[number + 1], 10, 10)
 end
